a9120e076e0cfd1162af0c037993b437e478e5c0
jecfmaatrixxx <jecfmaatrixxx@gmail.com>
2022-04-25 13:55:29 -0500

    homework01

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,11 +2,41 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  // algo
+  let numArray = [];
+  for (let i = 0; i < num.length; i++) {
+    numArray.push(num[i]) 
+  }
+
+  let dat_reverse = numArray.reverse();
+  let acum = 0;
+  
+  for (let i = 0; i < dat_reverse.length; i++) {
+    if(dat_reverse[i]!=0){
+      acum += Math.pow(2, i);  
+    }
+  }
+  return acum
 }
 
+DecimalABinario("4")
 function DecimalABinario(num) {
   // tu codigo aca
+  let numInt = Number(num);
+
+  let numArray = [];
+  let flag = true;
+  
+  do{
+      let round = Math.trunc(numInt/2);
+      let residue = numInt % 2
+      numArray.push(residue);
+      numInt = round;
+      if(numInt<=0){
+        flag = false;
+      }
+  }while(flag); 
+    return (numArray.reverse().join(''));
+
 }
 
 // No se pueden usar: 
