6926694457ecf77df8f36b6433768287d321865b
janunez8 <jecfmaatrixxx@gmail.com>
2022-05-03 12:51:06 -0500

    Homework 5

--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // resolve estos ejercicios usando recursión
 
 // Binary Seach Tree
@@ -13,56 +13,120 @@ function BinarySearchTree(value) {
   this.left = null;
   this.right = null;
 }
-function BinaryTree() {
-  this.root = null;
-}
 
-
-BinarySearchTree.prototype.insert = function(value){
-  if(value < this.value){
-    if(!this.left){
-      this.left = new BinarySearchTree(value)
-      return;
-    }else{
-      return this.left.insert(value)
+BinarySearchTree.prototype.insert = function (value) {
+  let node = new BinarySearchTree(value);
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = node;
+    } else {
+      this.left.insert(value);
     }
   }
-  if(value > this.value){
-    if(!this.right){
-      this.right = new BinarySearchTree(value)
-      return;
-    }else{
-      return this.right.insert(value)
+  if (value > this.value) {
+    if (!this.right) {
+      this.right = node;
+    } else {
+      this.right.insert(value);
     }
   }
+};
 
+BinarySearchTree.prototype.contains = function (value) {
+  if (value === this.value) return true;
+  if (value < this.value) {
+    if (!this.left) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else {
+    if (!this.right) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
+};
 
+//La posiscion en donde esta parado
+function mostrar(value) {
+  console.log(value);
 }
-var node = new BinarySearchTree()
-var tree = new BinaryTree()
 
-console.log(node.insert(5))
-console.log(node.insert("asd"))
+BinarySearchTree.prototype.depthFirstForEach = function (cb, route) {
+  
+  if(!route || route === "in-order"){
+    if (this.left) {
+      this.left.depthFirstForEach(cb, route);
+    }
+    cb(this.value);
+    if (this.right) {
+      this.right.depthFirstForEach(cb, route);
+    }
+  }else if(route === "pre-order"){
+    cb(this.value);
+    if (this.left) {
+      this.left.depthFirstForEach(cb, route);
+    }
 
-console.log(node.insert(10))
-console.log(node.insert(9))
+    if (this.right) {
+      this.right.depthFirstForEach(cb, route);
+    }
+  }else if(route === "post-order"){
+    if (this.left) {
+      this.left.depthFirstForEach(cb, route);
+    }
+    if (this.right) {
+      this.right.depthFirstForEach(cb, route);
+    }
+    cb(this.value);
+  }
+}
 
-tree.root = node;
-console.log(tree)
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, array) {
+  if (!array) {
+    var array = [];
+  }
+
+  if (this.left) {
+    array.push(this.left);
+  }
+  if (this.right) {
+    array.push(this.right);
+  }
+  cb(this.value);
+  if (array.length > 0) {
+    array.shift().breadthFirstForEach(cb, array);
+  }
+};
+
+BinarySearchTree.prototype.size = function () {
+  if (!this.left && !this.right) {
+    return 1;
+  } else if (this.left && this.right)
+    return 1 + this.left.size() + this.right.size();
+  else if (this.left) return 1 + this.left.size();
+  else if (this.right) return 1 + this.right.size();
+};
+
+var tree = new BinarySearchTree(10);
+
+tree.insert(25);
+tree.insert(4);
+
+console.log(tree.contains(1));
+
+tree.insert(3);
+
+//tree.depthFirstForEach("in-order");
 
 
-
-
-
-console.log(tree)
-
-
-
-
+console.log(tree);
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
